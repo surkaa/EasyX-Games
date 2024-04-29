cae4c8c5d05500321a9e226e065e31e9ae8f3059
SurKaa <98200894+surkaa@users.noreply.github.com>
2024-04-29 22:37:13 +0800

    移动的园

--- EasyXDemo/FileName.cpp
+++ EasyXDemo/FileName.cpp
@@ -2,6 +2,28 @@
 #include<iostream>
 
 int main() {
-	printf("%ws", GetEasyXVer());
+	initgraph(1280, 720);
+	int x = 100, y = 100;
+
+	BeginBatchDraw();
+
+	while (true)
+	{
+		ExMessage msg;
+		while (peekmessage(&msg))
+		{
+			if (msg.message == WM_MOUSEMOVE)
+			{
+				x = msg.x;
+			    y = msg.y;
+			}
+		}
+
+		cleardevice();
+		solidcircle(x, y, 100);
+		FlushBatchDraw();
+	}
+
+	EndBatchDraw();
 	return 0;
 }
